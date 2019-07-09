67b157306953840149c46f7aea276afd6b2a1b64
Leon Lang <leon.lang93@gmail.com>
2019-07-09 20:29:04 +0200

    add vector quantization in models

---
+++ baseline/models/vector_quantization.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+
+class VectorQuantization(torch.autograd.Function):
+    """
+    A function that compares the input of the forward pass to the embedding table.
+    returns the closest embedding vector.
+    Backward pass is straight-through.
+    Inspired by VQ-VAE (van den Oord et al., 2018).
+    Implementation adapted from https://github.com/ritheshkumar95/pytorch-vqvae/blob/master/functions.py.
+    """
+    @staticmethod
+    def forward(ctx, pre_quant, e):
+        # Use ||a - b||^2 = ||a||^2 + ||b||^2 - 2ab for computation of distances.
+        # Square computation:
+        e_sq = torch.sum(e ** 2, dim=1)
+        pre_quant_sq = torch.sum(pre_quant ** 2, dim=1, keepdim=True)
+
+        # Compute the distances to the codebook e
+        distances = torch.addmm(e_sq + pre_quant_sq,
+            pre_quant, e.t(), alpha=-2.0, beta=1.0)
+
+        _, indices_flatten = torch.min(distances, dim=1) # indices_flatten lists, for each vector in the batch pre_quant, the index of the closest codeword
+
+        return e[indices]
+
+    @staticmethod
+    def backward(ctx, grad_out):
+        return grad_out
