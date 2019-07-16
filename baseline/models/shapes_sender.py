cleaned up vocab_size problem. Made sure that gradient of distance function is correct

baseline/models/shapes_sender.py
@@ -29,11 +29,17 @@ class ShapesSender(nn.Module):
         inference_step=False,
         vqvae=False, # If True, use VQ instead of Gumbel Softmax
         discrete_latent_number=25, # Number of embedding vectors e_i in embedding table in vqvae setting
+        discrete_latent_dimension=25, #dimension of embedding vectors
         beta=0.25, # Weighting of loss terms 2 and 3 in VQ-VAE
         discrete_communication=False
         ):
 
         super().__init__()
+        if vqvae:
+            if discrete_communication==False:
+                assert vocab_size==discrete_latent_dimension, "When using continuous communication, vocab_size = discrete_latent_dimension"
+            else:
+                assert vocab_size==discrete_latent_number, "When using discrete communication, vocab_size = discrete_latent_number"
         self.vocab_size = vocab_size
         self.cell_type = cell_type
         self.output_len = output_len
@@ -64,16 +70,21 @@ class ShapesSender(nn.Module):
             torch.empty((vocab_size, embedding_size), dtype=torch.float32)
         )
 
-        self.linear_out = nn.Linear(hidden_size, vocab_size) # from a hidden state to the vocab
+        if not vqvae:
+            self.linear_out = nn.Linear(hidden_size, vocab_size) # from a hidden state to the vocab
+        else:
+            self.linear_out = nn.Linear(hidden_size, discrete_latent_dimension)
         self.vqvae = vqvae
 
         if self.vqvae:
             self.discrete_latent_number = discrete_latent_number
+            self.discrete_latent_dimension = discrete_latent_dimension
             self.e = nn.Parameter(
-                torch.empty((self.discrete_latent_number, self.vocab_size), dtype=torch.float32)
+                torch.empty((self.discrete_latent_number, self.discrete_latent_dimension), dtype=torch.float32)
             ) # The discrete embedding table
             print("the shape of e is {}".format(self.e.shape))
             self.vq = VectorQuantization()
+            self.discrete_communication=discrete_communication
             if self.discrete_communication:
                 self.hard_max = HardMax()
 
@@ -81,7 +92,6 @@ class ShapesSender(nn.Module):
             self.reset_parameters()
 
         self.beta=beta
-        self.discrete_communication=discrete_communication
 
     def reset_parameters(self):
         nn.init.normal_(self.embedding, 0.0, 0.1)
@@ -230,6 +240,7 @@ class ShapesSender(nn.Module):
             else:
                 pre_quant = self.linear_out(h)
                 indices = [None] * batch_size
+
                 if not self.discrete_communication:
                     token = self.vq.apply(pre_quant, self.e, indices)
                     #print_indices = [0, 1, 2]
@@ -237,9 +248,9 @@ class ShapesSender(nn.Module):
                 else:
                     distances = distance_computer(pre_quant)
                     softmin = F.softmax(-distances, dim=1)
-                    token = self.hard_max(softmin, indices) # This also updates the indices
-                    print_indices = [0, 1, 2]
-                    print(np.array(indices)[print_indices])
+                    token = self.hard_max.apply(softmin, indices, self.discrete_latent_number) # This also updates the indices
+                    #print_indices = [0, 1, 2]
+                    #print(np.array(indices)[print_indices])
                 loss_2 = torch.mean(torch.norm(pre_quant.detach() - self.e[indices], dim=1)**2)
                 loss_3 = torch.mean(torch.norm(pre_quant - self.e[indices].detach(), dim=1)**2)
                 loss_2_3 = loss_2 + self.beta*loss_3 # This corresponds to the second and third loss term in VQ-VAE

baseline/models/vector_quantization.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
-from torch.nn import Functional as F
+from torch.nn import functional as F
+from torch.autograd import Variable
 
 class HardMax(torch.autograd.Function):
     """
@@ -8,7 +9,7 @@ class HardMax(torch.autograd.Function):
     With straight-through gradient.
     """
     @staticmethod
-    def forward(ctx, softmax, max_indices):
+    def forward(ctx, softmax, max_indices, n_dims):
 
         # this utils function is taken from https://discuss.pytorch.org/t/convert-int-into-one-hot-format/507/23
         def to_one_hot(y, n_dims=None):
@@ -21,13 +22,15 @@ class HardMax(torch.autograd.Function):
             return Variable(y_one_hot) if isinstance(y, Variable) else y_one_hot
 
         _, max_indices[:] = torch.max(softmax, dim=1)
-        hard_max = to_one_hot(max_indices)
+        hard_max = to_one_hot(torch.Tensor(max_indices), n_dims)
 
         return hard_max
 
     @staticmethod
     def backward(ctx, grad_hard_max):
-        return grad_hard_max
+        return grad_hard_max, None, None
+
+
 
 class VectorQuantization(torch.autograd.Function):
     """
@@ -50,10 +53,12 @@ class VectorQuantization(torch.autograd.Function):
     def backward(ctx, grad_e):
         return grad_e, None, None
 
+
+
 class EmbeddingtableDistances(nn.Module):
     def __init__(self, e):
         super().__init__()
-        self.e = e # The embedding table from VQ-VAE
+        self.register_buffer('e', e) # The embedding table from VQ-VAE
 
     def forward(self, pre_quant):
         # Use ||a - b||^2 = ||a||^2 + ||b||^2 - 2ab for computation of distances.