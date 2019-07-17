ed7c75f8d87b5f46281c29538226a871e6692edf
Leon Lang <leon.lang93@gmail.com>
2019-07-17 14:02:26 +0200

    activate venv in job file

--- jobs/test-vqvae.sh
+++ jobs/test-vqvae.sh
@@ -7,12 +7,15 @@
 #SBATCH --mem=60000M
 #SBATCH --partition=gpu_shared_course
 #SBATCH --gres=gpu:1
-module purge
-module load eb
-module load Python/3.6.3-foss-2017b
-module load cuDNN/7.0.5-CUDA-9.0.176
-module load NCCL/2.0.5-CUDA-9.0.176
-export LD_LIBRARY_PATH=/hpc/eb/Debian9/cuDNN/7.1-CUDA-8.0.44-GCCcore-5.4.0/lib64:$LD_LIBRARY_PATH
+
+#module purge
+#module load eb
+#module load Python/3.6.3-foss-2017b
+#module load cuDNN/7.0.5-CUDA-9.0.176
+#module load NCCL/2.0.5-CUDA-9.0.176
+#export LD_LIBRARY_PATH=/hpc/eb/Debian9/cuDNN/7.1-CUDA-8.0.44-GCCcore-5.4.0/lib64:$LD_LIBRARY_PATH
+
+source vqvae/bin/activate
 
 python3 -u baseline/train_game.py \
 	--device cpu \
